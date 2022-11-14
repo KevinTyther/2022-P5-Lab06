5f2c61eab140522f7f353fe6df84f5adb099ee45
Kevin Tyther <kevin.tyther@vtsd.com>
2022-11-14 13:50:02 -0500

    Lab06

--- Lab06vst.java
+++ Lab06vst.java
@@ -19,14 +19,47 @@ public class Lab06vst extends Applet
 
 
         // Draw Random Lines
+        Random rand = new Random();
+        int linex = rand.nextInt(150)+20;
+        int linex2 = rand.nextInt(390)+10;
+        int liney = rand.nextInt(100)+20;
+        int liney2 = rand.nextInt(290)+10;
+        int R = rand.nextInt(255);
+        int G = rand.nextInt(255);
+        int B = rand.nextInt(255);
 
-
-
+        for (int k = 0; k < 100; k++)
+        {
+            g.setColor(new Color(R,G,B));
+            g.drawLine(linex, liney, linex2, liney2);
+            linex = rand.nextInt(150)+20;
+            linex2 = rand.nextInt(390)+10;
+            liney = rand.nextInt(100)+20;
+            liney2 = rand.nextInt(290)+10;
+            R = rand.nextInt(255);
+            G = rand.nextInt(255);
+            B = rand.nextInt(255);
+        }
 
 
         // Draw Random Squares
 
-
+        int sqrx = rand.nextInt(250)+450;
+        int sqry = rand.nextInt(150)+50;
+        int sqrH = rand.nextInt(50)+10;
+        int sqrW = rand.nextInt(50)+10;
+        for (int k = 0; k < 100; k++)
+        {
+            g.setColor(new Color(R,G,B));
+            g.fillRect(sqrx,sqry,sqrW, sqrH);
+            sqrx = rand.nextInt(250)+450;
+            sqry = rand.nextInt(150)+50;
+            sqrH = rand.nextInt(50)+10;
+            sqrW = rand.nextInt(50)+10;
+            R = rand.nextInt(255);
+            G = rand.nextInt(255);
+            B = rand.nextInt(255);
+        }
 
 
 
