40d97b1530d573b1a20850983aa1d1542ac6ef11
Kevin Tyther <kevin.tyther@vtsd.com>
2022-11-16 12:28:06 -0500

    Lab06

--- Lab06vst.java
+++ Lab06vst.java
@@ -20,42 +20,38 @@ public class Lab06vst extends Applet
 
         // Draw Random Lines
         Random rand = new Random();
-        int linex = rand.nextInt(150)+20;
-        int linex2 = rand.nextInt(390)+10;
-        int liney = rand.nextInt(100)+20;
-        int liney2 = rand.nextInt(290)+10;
+
         int R = rand.nextInt(255);
         int G = rand.nextInt(255);
         int B = rand.nextInt(255);
 
         for (int k = 0; k < 100; k++)
         {
-            g.setColor(new Color(R,G,B));
-            g.drawLine(linex, liney, linex2, liney2);
-            linex = rand.nextInt(150)+20;
-            linex2 = rand.nextInt(390)+10;
-            liney = rand.nextInt(100)+20;
-            liney2 = rand.nextInt(290)+10;
+            int lineX = rand.nextInt(391)+10;
+            int lineY = rand.nextInt(291)+10;
+            int lineX2 = rand.nextInt(391)+10;
+            int lineY2 = rand.nextInt(291)+10;
             R = rand.nextInt(255);
             G = rand.nextInt(255);
             B = rand.nextInt(255);
+            g.setColor(new Color(R,G,B));
+            g.drawLine(lineX, lineY,lineX2, lineY2);
         }
 
 
         // Draw Random Squares
 
-        int sqrx = rand.nextInt(250)+450;
-        int sqry = rand.nextInt(150)+50;
-        int sqrH = rand.nextInt(50)+10;
-        int sqrW = rand.nextInt(50)+10;
+
+        int sqrx = rand.nextInt(340)+400;
+        int sqry = rand.nextInt(240)+10;
+        int sqrH = 50;
+        int sqrW = 50;
         for (int k = 0; k < 100; k++)
         {
             g.setColor(new Color(R,G,B));
             g.fillRect(sqrx,sqry,sqrW, sqrH);
-            sqrx = rand.nextInt(250)+450;
-            sqry = rand.nextInt(150)+50;
-            sqrH = rand.nextInt(50)+10;
-            sqrW = rand.nextInt(50)+10;
+            sqrx = rand.nextInt(340)+400;
+            sqry = rand.nextInt(240)+10;
             R = rand.nextInt(255);
             G = rand.nextInt(255);
             B = rand.nextInt(255);
@@ -65,14 +61,46 @@ public class Lab06vst extends Applet
 
         // Draw Random Circles
 
+        for (int k = 0; k < 100; k++)
+        {
+            int diameter = rand.nextInt(200);
+            int cirX = rand.nextInt(391-diameter)+10;
+            int cirY = rand.nextInt(291-diameter)+300;
+            R = rand.nextInt(255);
+            G = rand.nextInt(255);
+            B = rand.nextInt(255);
+            g.setColor(new Color(R,G,B));
+            g.drawOval(cirX, cirY, diameter, diameter);
 
+        }
 
 
 
         // Draw 3-D Box
 
+        Polygon poly = new Polygon();
+        Polygon poly1 = new Polygon();
+        Polygon poly2 = new Polygon();
+        Polygon poly3 = new Polygon();
+        poly.addPoint(500,400);
+        poly.addPoint(550,400);
+        poly.addPoint(550,450);
+        poly.addPoint(500,450);
 
+        poly1.addPoint(525,425);
+        poly1.addPoint(575,425);
+        poly1.addPoint(575,475);
+        poly1.addPoint(525,475);
 
+        poly2.addPoint(550,425);
+        poly2.addPoint(550,450);
+        poly2.addPoint(575,450);
+        g.setColor(Color.YELLOW);
+        g.fillPolygon(poly);
+        g.setColor(Color.RED);
+        g.fillPolygon(poly1);
+        g.setColor(Color.BLUE);
+        g.fillPolygon(poly2);
 
 
     }
